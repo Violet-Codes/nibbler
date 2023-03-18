89435c11a5fdda6a7c1ad3b0fe1fdafffdbba441
Violet-Codes <125933964+Violet-Codes@users.noreply.github.com>
2023-03-18 17:44:11 +0000

    resolved infinite type issue with recursive combinators using inner functions

--- src/combinators.rs
+++ src/combinators.rs
@@ -6,17 +6,26 @@ pub const fn most_till<Iter, Err, T, U>(
 )
     -> parser![Iter, Vec<Err>, (Vec<T>, U)]
 {
-    move |iter| otherwise(
-        fmap2(
-            |t, (mut ts, u) | { ts.insert(0, t); (ts, u) },
-            wrap_err(& parser),
-            most_till(& parser, & end)
-        ),
-        fmap(
-            |u| (vec![], u),
-            wrap_err(& end)
-        )
-    )(iter)
+    fn most_till_inner<Iter_, Err_, T_, U_>(
+        parser_: parser![Iter_, Err_, T_],
+        end_: parser![Iter_, Err_, U_],
+        iter: &mut Iter_
+    )
+        -> Result<(Vec<T_>, U_), Vec<Err_>>
+    {
+        otherwise(
+            fmap2(
+                |t, (mut ts, u): (Vec<T_>, U_)| { ts.insert(0, t); (ts, u) },
+                wrap_err(& parser_),
+                |iter| most_till_inner::<Iter_, Err_, T_, U_>(& parser_, & end_, iter)
+            ),
+            fmap(
+                |u| (vec![], u),
+                wrap_err(& end_)
+            )
+        )(iter)
+    }
+    move |iter| most_till_inner(& parser, & end, iter)
 }
 
 pub const fn least_till<Iter, Err, T, U>(
@@ -25,17 +34,26 @@ pub const fn least_till<Iter, Err, T, U>(
 )
     -> parser![Iter, Vec<Err>, (Vec<T>, U)]
 {
-    move |iter| otherwise(
-        fmap(
-            |u| (vec![], u),
-            wrap_err(& end)
-        ),
-        fmap2(
-            |t, (mut ts, u) | { ts.insert(0, t); (ts, u) },
-            wrap_err(& parser),
-            least_till(& parser, & end)
-        )
-    )(iter)
+    fn least_till_inner<Iter_, Err_, T_, U_>(
+        parser_: parser![Iter_, Err_, T_],
+        end_: parser![Iter_, Err_, U_],
+        iter: &mut Iter_
+    )
+        -> Result<(Vec<T_>, U_), Vec<Err_>>
+    {
+        otherwise(
+            fmap(
+                |u| (vec![], u),
+                wrap_err(& end_)
+            ),
+            fmap2(
+                |t, (mut ts, u): (Vec<T_>, U_)| { ts.insert(0, t); (ts, u) },
+                wrap_err(& parser_),
+                |iter| least_till_inner::<Iter_, Err_, T_, U_>(& parser_, & end_, iter)
+            )
+        )(iter)
+    }
+    move |iter| least_till_inner(& parser, & end, iter)
 }
 
 pub const fn most<Iter, Err, T>(
